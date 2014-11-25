6720b13a7564ef4806b8d0f1df237bc460857535
Julio Silva <julio@juliobs.com>
2014-11-25 03:21:52 +0100

    Helper funcion to print vectors

---
+++ Print_Vector.hpp
@@ -0,0 +1,33 @@
+// ============================================================================
+//
+//       Filename:  Print_Vector.hpp
+//
+//    Description:  Print Vector
+//                  Print a vector with elements separated by a comma and space
+//
+//        Version:  1.0
+//        Created:  06/Nov/2014 20:24:34
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Julio Batista Silva (342639), julio@juliobs.com
+//        Company:  RWTH Aachen
+//
+// ============================================================================
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+template<typename T>
+void Print_Vector(vector<T> v)
+{
+    typename vector< T >::iterator it = v.begin();
+    cout << *it;
+
+    for (++it; it != v.end(); ++it)
+        cout << " " << *it;
+
+    cout << endl;
+}
