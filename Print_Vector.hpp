86776dc557faf58c15c83a2c338a0c6cee3032bc
Julio Silva <julio@juliobs.com>
2014-11-29 16:50:50 +0100

    New parameter separator

--- Print_Vector.hpp
+++ Print_Vector.hpp
@@ -3,7 +3,8 @@
 //       Filename:  Print_Vector.hpp
 //
 //    Description:  Print Vector
-//                  Print a vector with elements separated by a space
+//                  Print a vector with elements separated by 'separator'
+//                  (default: space)
 //
 //        Version:  1.0
 //        Created:  06/Nov/2014 20:24:34
@@ -21,13 +22,13 @@ using namespace std;
 
 
 template<typename T>
-void Print_Vector(vector<T> v)
+void Print_Vector(vector<T> v, const char separator = ' ')
 {
     typename vector< T >::iterator it = v.begin();
     cout << *it;
 
     for (++it; it != v.end(); ++it)
-        cout << " " << *it;
+        cout << separator << *it;
 
     cout << endl;
 }
