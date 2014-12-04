3867fdca655293c617fd9f90d8a8bc1e5db191ed
Julio Silva <julio@juliobs.com>
2014-12-04 02:56:44 +0100

    Make Expand_Peptide a separated function

--- Cyclopeptide_From_Ideal_Spectrum.hpp
+++ Cyclopeptide_From_Ideal_Spectrum.hpp
@@ -5,6 +5,8 @@
 //    Description:  Receives a ideal experimental spectrum and returns peptides
 //                  (in mass notation) that generate the same spectrum.
 //
+//                  TODO: Maybe lists are faster than vectors
+//
 //        Version:  1.0
 //        Created:  29/Nov/2014 19:36:19
 //       Revision:  none
@@ -20,31 +22,9 @@
 #include "Linear_Spectrum.hpp"
 #include "Peptide_Mass.hpp"
 #include "Peptide_Is_Consistent.hpp"
+#include "Expand_Peptide.hpp"
 
 
-// Expand each peptide by one
-void Expand_Peptide(std::vector< std::vector<int> > &peptides)
-{
-    std::vector<int> amino_acidos = {57, 71, 87, 97, 99, 101, 103, 113, 114,
-                                     115, 128, 129, 131, 137, 147, 156, 163, 186
-                                    };
-    std::vector< std::vector<int> > new_pep;
-
-    for (const auto p : peptides)
-    {
-        for (const auto am : amino_acidos)
-        {
-            std::vector<int> temp = p;
-            temp.push_back(am);
-            new_pep.push_back(temp);
-        }
-    }
-
-    peptides = new_pep;
-}
-
-
-// TODO: Maybe lists are faster than vectors
 std::vector< std::vector<int> > Cyclopeptide_From_Ideal_Spectrum(std::vector<int> spectrum)
 {
     std::vector< std::vector<int> > peptides = { {57}, {71}, {87}, {97}, {99},
@@ -57,6 +37,7 @@ std::vector< std::vector<int> > Cyclopeptide_From_Ideal_Spectrum(std::vector<int
     {
         Expand_Peptide(peptides);
 
+        // Iterates backwards to keep the iterator valid after erasing elements
         for (std::vector<std::vector <int> >::reverse_iterator p_it = peptides.rbegin(); p_it != peptides.rend(); ++p_it)
         {
             if (Peptide_Mass(*p_it) == spectrum.back()) // Equal masses
---
+++ Expand_Peptide.hpp
@@ -0,0 +1,38 @@
+// ============================================================================
+// 
+//       Filename:  Expand_Peptide.hpp
+// 
+//    Description:  Receives a vector of peptides (each as a vector of integer
+//                  masses) and adds one amino acid to each of those peptides
+// 
+//        Version:  1.0
+//        Created:  04/Dez/2014 02:49:43
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Julio Batista Silva (342639), julio@juliobs.com
+//        Company:  RWTH Aachen
+// 
+// ============================================================================
+#pragma once
+
+
+void Expand_Peptide(std::vector< std::vector<int> > &peptides)
+{
+    std::vector<int> amino_acidos = {57, 71, 87, 97, 99, 101, 103, 113, 114,
+                                     115, 128, 129, 131, 137, 147, 156, 163, 186
+                                    };
+    std::vector< std::vector<int> > new_pep;
+
+    for (const auto p : peptides)
+    {
+        for (const auto am : amino_acidos)
+        {
+            std::vector<int> temp = p;
+            temp.push_back(am);
+            new_pep.push_back(temp);
+        }
+    }
+
+    peptides = new_pep;
+}
