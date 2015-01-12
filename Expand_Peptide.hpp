b4622f46656739cd4ad6782e1a33807ac39ec55a
Julio Silva <julio@juliobs.com>
2015-01-12 04:19:49 +0100

    Function to expand peptides

--- Expand_Peptide.hpp
+++ Expand_Peptide.hpp
@@ -3,7 +3,8 @@
 //       Filename:  Expand_Peptide.hpp
 //
 //    Description:  Receives a vector of peptides (each as a vector of integer
-//                  masses) and adds one amino acid to each of those peptides
+//                  masses or as the Peptide type) and adds one amino acid to
+//                  each of those peptides
 //
 //        Version:  1.0
 //        Created:  04/Dez/2014 02:49:43
@@ -15,6 +16,7 @@
 //
 // ============================================================================
 #pragma once
+#include "Peptide.hpp"
 
 
 void Expand_Peptide(std::vector< std::vector<int> > &peptides)
@@ -22,7 +24,6 @@ void Expand_Peptide(std::vector< std::vector<int> > &peptides)
     std::vector<int> amino_acidos = {57, 71, 87, 97, 99, 101, 103, 113, 114,
                                      115, 128, 129, 131, 137, 147, 156, 163, 186
                                     };
-    std::vector< std::vector<int> > new_pep;
 
     if (peptides.empty())
     {
@@ -32,6 +33,8 @@ void Expand_Peptide(std::vector< std::vector<int> > &peptides)
     }
     else
     {
+        std::vector< std::vector<int> > new_pep;
+
         for (const auto p : peptides)
         {
             for (const auto am : amino_acidos)
@@ -41,6 +44,39 @@ void Expand_Peptide(std::vector< std::vector<int> > &peptides)
                 new_pep.push_back(temp);
             }
         }
+
+        peptides = new_pep;
+    }
+}
+
+
+// Overload
+void Expand_Peptide(std::vector<Peptide> &peptides, const std::vector<int> spectrum)
+{
+    std::vector<int> amino_acidos = {57, 71, 87, 97, 99, 101, 103, 113, 114,
+                                     115, 128, 129, 131, 137, 147, 156, 163, 186
+                                    };
+    std::vector< std::vector<int> > new_pep;
+
+    if (peptides.empty())
+    {
+        for (const auto am : amino_acidos)
+        {
+            std::vector<int> temp = {am};
+            peptides.push_back(Peptide(temp, spectrum));
+        }
+    }
+    else
+    {
+        std::vector<Peptide> new_pep;
+
+        for (const auto p : peptides)
+            for (const auto am : amino_acidos)
+            {
+                std::vector<int> m_temp = p.masses;
+                m_temp.push_back(am);
+                new_pep.push_back(Peptide(m_temp, spectrum));
+            }
 
         peptides = new_pep;
     }
